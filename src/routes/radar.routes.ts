b47b4bdaf451e5cf2c769b92326b36b7fcbf5477
kikeiz <kikeizp96@gmail.com>
2023-08-02 22:33:29 +0200

    last

--- src/routes/radar.routes.ts
+++ src/routes/radar.routes.ts
@@ -1,12 +1,12 @@
 const express = require('express');
 import { Application } from "express";
 import { RadarController } from "../controllers/radar.controller";
+import { MAIN_ROUTE } from "../constants/constants";
 
 export class RadarRoutes{
   
     private radarController: RadarController = new RadarController()
     public routes(app: Application){
-        app.route('/radar').post(this.radarController.targetCoordinates);
-
+        app.route(`/${MAIN_ROUTE}/radar`).post(this.radarController.targetCoordinates);
     }   
 }
